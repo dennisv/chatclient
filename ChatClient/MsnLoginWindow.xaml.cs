dbc5f65158de36420f79ca2d8b4beb1c6e4a5a72
Dennis Vermeulen <blacktorn@gmail.com>
2010-01-27 10:36:07 +0100

    You can now login to MSN

--- ChatClient/MsnLoginWindow.xaml.cs
+++ ChatClient/MsnLoginWindow.xaml.cs
@@ -7,9 +7,10 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+
+using MSNPSharp;
+using MSNPSharp.Core;
 
 namespace ChatClient
 {
@@ -18,9 +19,41 @@ namespace ChatClient
     /// </summary>
     public partial class MsnLoginWindow : Window
     {
+        private Messenger messenger = new Messenger();
         public MsnLoginWindow()
         {
             InitializeComponent();
+
+            messenger.Nameserver.SignedIn += new EventHandler<EventArgs>(Nameserver_SignedIn);
+            messenger.Nameserver.AuthenticationError += new EventHandler<ExceptionEventArgs>(Nameserver_AuthenticationError);
+        }
+
+        void Nameserver_AuthenticationError(object sender, ExceptionEventArgs e)
+        {
+            MessageBox.Show("Authentication failed, check your emailaddress or password.", e.Exception.InnerException.Message);
+            btnLogin.Content = "Login";
+            btnLogin.IsEnabled = true;
+        }
+
+        void Nameserver_SignedIn(object sender, EventArgs e)
+        {
+            MessageBox.Show("Success!");
+            messenger.Owner.Status = PresenceStatus.Online;
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbEmailaddress.Text.Trim() == "" || tbPassword.Password.Trim() == "")
+            {
+                MessageBox.Show("Please fill in both your emailaddress and password.");
+                return;
+            }
+
+            messenger.Credentials = new Credentials(tbEmailaddress.Text, tbPassword.Password, MsnProtocol.MSNP18);
+
+            btnLogin.Content = "Connecting...";
+            btnLogin.IsEnabled = false;
+            messenger.Connect();
         }
     }
 }
